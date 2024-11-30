1b3cdb9c8a920f9a871a7086d0509cf4175e09d9
fire833 <kttpsy@gmail.com>
2024-11-30 00:54:51 -0600

    fix: remove old ubuntu script, this was moved on accident

--- bingraph/ubuntu_init.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# This script should only be run using the build_gallery.sh script. It's a wrapper to initalize the ubuntu container correctly.
-
-apt update && apt upgrade -y && apt install -y ssh gcc curl wget graphviz net-tools
-
-./data/bingraph -b /usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin -l /lib:/lib64:/usr/local/lib:/usr/lib:/usr/lib64 -o /data/ubuntu$1.json --output-graphviz /data/ubuntu$1.dot
